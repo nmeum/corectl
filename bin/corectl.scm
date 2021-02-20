28b689574cdaad9e0a715423dc6e3cdb69348c1f
Sören Tempel <soeren+git@soeren-tempel.net>
2021-02-20 01:36:03 +0100

    Add corectl program component

---
+++ bin/corectl.scm
@@ -0,0 +1,49 @@
+(import scheme
+        (chicken base)
+        (chicken process-context)
+        (chicken string)
+        corectl)
+
+(define (string->led s)
+  (define (led-num x)
+    (if (number? x) x
+      (let* ((lst1 (string-split x "LED"))
+             (lst2 (string-split x "led"))
+             (num  (cond
+                     ((eq? (length lst1) 1) (car lst1))
+                     ((eq? (length lst2) 1) (car lst2))
+                     (else #f))))
+        (if num (string->number num) #f))))
+
+  (let ((n (led-num s)))
+    (if (and n (< n 6))
+      n
+      (error "unknown LED"))))
+
+(define (string->state s)
+  (cond
+    ((equal? s "on")      'on)
+    ((equal? s "off")     'off)
+    ((equal? s "restore") 'restore)
+    (else (error "invalid LED state"))))
+
+(define (parse-leds input)
+  (map
+    (lambda (x)
+      (let ((pair (string-split x ":")))
+        (if (not (eq? (length pair) 2))
+          (error "invalid input pair")
+          (cons
+            (string->led (car pair))
+            (string->state (cadr pair))))))
+    input))
+
+(define (main)
+  (let* ((leds (parse-leds (command-line-arguments)))
+         (ctrl (make-led-control)))
+    (write-leds ctrl leds)
+    (close-led-control ctrl)))
+
+(cond-expand
+  ((or chicken-script compiling) (main))
+  (else #t))
